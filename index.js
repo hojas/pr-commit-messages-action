d498d46870df59c569bcbb35ebd203fceacc3c5a
hojas <cyxhuan9@163.com>
2024-02-23 11:37:46 +0800

    fix commits

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+import core from '@actions/core'
+import github from '@actions/github'
 
 try {
   const { payload } = github.context
-  const commits = payload.commits
-    .map(c => c.message)
-    .filter(m => !/^Merge pull request/.test(m))
-    .map(m => m.replace(/\n+(.*)/g, '\n> $1'))
-    .map(m => `> ${m}`)
-    .join('\n')
+  const { commits } = payload
+  console.log('payload', payload)
+  console.log('commits', commits)
 
-  core.setOutput('commits', commits)
-} catch (error) {
+  if (!commits) {
+    core.setOutput('commits', 'No commit')
+  }
+  else {
+    const commits = payload.commits
+      .map(c => c.message)
+      .filter(m => !m.startsWith('Merge pull request'))
+      .map(m => m.replace(/\n+(.*)/g, '\n> $1'))
+      .map(m => `> ${m}`)
+      .join('\n')
+
+    core.setOutput('commits', commits)
+  }
+}
+catch (error) {
   core.error(error.message)
 }
